d43416056f381956b90d58a85c3f51fcb8c01341
abootatoo <42549471+abootatoo@users.noreply.github.com>
2018-10-24 08:02:28 -0700

    Update Farm.java

--- Farm.java
+++ Farm.java
@@ -3,7 +3,7 @@ class Farm
   private Animal[] aBunchOfAnimals = new Animal[3];    
     public Farm()     
     {       
-       aBunchOfAnimals[0] = new NamedCow("cow","moo","Elsie");          
+       aBunchOfAnimals[0] = new NamedCow("Elsie","cow","moo");          
        aBunchOfAnimals[1] = new Chick("chick","cheep","cluck");
        aBunchOfAnimals[2] = new Pig("pig","oink");    
     }     
